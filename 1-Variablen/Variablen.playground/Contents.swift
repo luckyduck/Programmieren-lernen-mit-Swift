f9b67862cf33ff4023f02097cf6fa4b2511b2fb1
Jan Brinkmann <lucky@the-luckyduck.de>
2015-11-30 23:09:14 +0100

    init

---
+++ 1-Variablen/Variablen.playground/Contents.swift
@@ -0,0 +1,16 @@
+//
+//
+// Variablen
+var meineVariable = 14
+meineVariable = 23
+meineVariable = 101
+// Fehler: meineVariable = "Jan"
+
+var tr = "Mein Name ist Jan"
+var pi = 3.14
+
+//
+//
+// Konstanten
+let meineKonstante = 14
+//meineKonstante = 23
