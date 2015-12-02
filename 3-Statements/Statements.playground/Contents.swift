a5dc4bdf78c4de34d8507db71a968b6b1015400f
Jan Brinkmann <lucky@the-luckyduck.de>
2015-12-02 21:54:46 +0100

    statements

---
+++ 3-Statements/Statements.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+
+var str = "Hello, playground"
+
+for char in str.characters {
+    print(char)
+}
+
+var x = 2; var y = 3
+var summe = x + y
+
+func multiplizieren(x: Int, y: Int) -> Int {
+    let myResult = x * y
+    return myResult
+}
+
+multiplizieren(2, y: 5)
+
+if summe > 10 {
+    print(summe)
+}
+
+
+
