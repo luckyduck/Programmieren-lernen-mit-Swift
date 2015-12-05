386f9555741c357a7ad73c32964ce8f6c78379b9
Jan Brinkmann <lucky@the-luckyduck.de>
2015-12-05 22:34:57 +0100

    Rechnen

---
+++ 6-Rechnen/Rechnen.playground/Contents.swift
@@ -0,0 +1,31 @@
+// EAV
+
+let x = 3
+let y = 2
+
+// addieren
+let sum = x + y
+
+// subtrahieren
+let diff = x - y
+
+// multiplizieren
+let z = x * y
+
+// divieren
+let abc = Double(x) / Double(y)
+
+// modulo
+let zahlA = 1.5
+let zahlB = 0.4
+let rest = zahlA % zahlB
+
+let myNumber = 7
+let isGerade = myNumber % 2
+
+// klammern
+// 5 + 12 => 17
+let inKorrekt = 5 + 4 * 3
+
+// 5 + 4 => 9 * 3 => 27
+let korrekt = (5 + 4) * 3
