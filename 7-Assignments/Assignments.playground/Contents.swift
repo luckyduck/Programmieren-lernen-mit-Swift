e026cc1eee66adb89976d695db05c9d23cba9c84
Jan Brinkmann <lucky@the-luckyduck.de>
2015-12-07 22:23:07 +0100

    assignments

---
+++ 7-Assignments/Assignments.playground/Contents.swift
@@ -0,0 +1,24 @@
+var x = 10
+var y = 5
+var z = y
+x = y
+
+/* falsch:
+if a = y {
+    
+}
+*/
+
+var abc = 2.0
+
+// abc = abc + 4
+abc += 4
+
+// abc = abc - 1
+abc -= 1
+
+// abc = abc * 2
+abc *= 2
+
+// abc = abc / 4
+abc /= 4.0
